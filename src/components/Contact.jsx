f453d51d0833be6fc15133dfcf72575de1ee0850
Ashwini Salunke <162668451+AshwiniScripter@users.noreply.github.com>
2025-05-28 12:09:57 +0530

    initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center">
         
         {/* Contact Information Section */}
-        <div className="w-full md:w-1/2 h-auto rounded-3xl mx-4 ms-[120px] mt-[80px] shadow-lg p-6 border bg-gray-900 border-gray-600 flex  flex-col items-center max-w-[400px]">
+        <div className="w-full md:w-1/2 h-auto rounded-3xl mx-4 ms-[160px] mt-[80px] shadow-lg p-6 border bg-gray-900 border-gray-600 flex  flex-col items-center max-w-[400px]">
           <h1 className="text-2xl font-semibold text-center mb-6">ASHWINI SALUNKE</h1>
 
           {/* Email Section */}
@@ -45,7 +45,7 @@ const Contact = () => {
         </div>
 
         {/* Contact Form Section */}
-        <div className="w-full md:w-1/2 flex justify-center">
+        <div className="w-full md:w-1/2 flex justify-center max-w-[500px]">
           <div className="text-center">
             <p className="text-lg text-white mb-8">
               If you have any questions or would like to get in touch, feel free to send me a message.
